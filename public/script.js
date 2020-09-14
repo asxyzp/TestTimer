ec78cdbdfdb335cb2144bae46f5e51e31ccff28e
asxyzp <asxyzp@gmail.com>
2020-09-14 23:41:02 +0530

    Timer elapsed visualization & functionality for reload button

--- public/script.js
+++ public/script.js
@@ -1,8 +1,11 @@
 let time = 25;  //minutes
 $(document).ready(function(){
 
-    $(".pause").prop("disabled",true);  //Pause button is disabled by default
-    
+    $(".restart").prop("disabled",true);  //Pause button is disabled by default
+    $(".restart").click(function(){
+        location.reload();
+    });
+
     $(".time").html(`${time}`);         //Time div shows the default time
 
     //Increasing time
@@ -20,34 +23,33 @@ $(document).ready(function(){
     let timerStartTime;     //Stores time at which timer starts
     let totalTime;          //Stores total time for the timer
     let currentTime;        //Stores current time
-    let blinkCount = 0;     //Counts how many times the counter blinks
+    let timeElapsed;        //Stores the amount of time which has elapsed
+    let timeElapsedPercent; //Stores time which has elapsed
     
     $(".start").click(function(){
         
+        //Changing properties of buttons
         $(".pause").prop("disabled",false);     //Activates the pause button
         $(".inc").prop("disabled",true);        //Disables increase button
         $(".dec").prop("disabled",true);        //Disables decrease button
         $(this).prop("disabled",true);          //Disables start button
-
-        timerStartTime = Date.now();
-        totalTime = Number($(".time").html())*60000;
         
 
-        let timer = window.setInterval(()=>{
-            
-            //Adding basic animation to make it appear the time text blink
-            if(blinkCount%2==0){
-                $(".time").css("transform","scale(0.9)");
-                $(".min").css("transform","scale(0.9)");
-                ++blinkCount;
-            }
-            else{
-                $(".time").css("transform","scale(1)");
-                $(".min").css("transform","scale(1)");
-                ++blinkCount;
-            }
+        timerStartTime = Date.now();            //Records time at which timer has started
+        totalTime = Number($(".time").html())*60000;    //Records total time for which timer should run 
+        
+        //Checks the elapsed time every 1s
+        let timer = setInterval(()=>{
             currentTime = Date.now();
-            
+            timeElapsed = currentTime-timerStartTime;
+            timeElapsedPercent = (timeElapsed/totalTime)*100;
+            console.log(timeElapsedPercent);
+            if(timeElapsed>totalTime){
+                //Changing properties of buttons
+                $(".restart").prop("disabled",false);     //Activates the pause button
+                clearInterval(timer);
+            }
+            $(".timer-bg").css("height",`${timeElapsedPercent}vh`);
         },1000);
     });
 });

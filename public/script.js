4572adbe34c9734856ac66209129fab401984eae
asxyzp <asxyzp@gmail.com>
2020-09-14 21:02:52 +0530

    Added interactivity
    Timer time can be increased or decreased by clicking the button & disabling all buttons when start button is clicked

--- public/script.js
+++ public/script.js
@@ -1,3 +1,17 @@
+let time = 25;  //minutes
 $(document).ready(function(){
-    
+    $(".time").html(`${time}`);
+    $(".inc").click(function(){
+        ++time;
+        $(".time").html(`${time}`);   
+    });
+    $(".dec").click(function(){
+        --time;
+        $(".time").html(`${time}`);    
+    });
+    $(".start").click(function(){
+        $(".inc").prop("disabled",true);
+        $(".dec").prop("disabled",true);
+        $(this).prop("disabled",true);
+    });
 });
